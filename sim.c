5ed058f4cb1f794cb68b6eed8b5855081c75e6c0
Hustcketlyj <40982662+Hustcketlyj@users.noreply.github.com>
2022-04-09 15:48:17 +0800

    Update sim.c

--- sim.c
+++ sim.c
@@ -571,7 +571,7 @@ void handle_lw(unsigned int cur_inst) {
      */
     unsigned int rd = MASK11_7(cur_inst), rs1 = MASK19_15(cur_inst);
     int imm12 = MASK31_20(cur_inst);
-    NEXT_LATCHES.REGS[rd] = sext(MASK31_0(MEMORY[sext(imm12, 12) + CURRENT_LATCHES.REGS[rs1]]), 32);
+    NEXT_LATCHES.REGS[rd] = sext(MEMORY[sext(imm12, 12) + CURRENT_LATCHES.REGS[rs1]], 32);
     //warn("Lab2-2 assignment: LW\n");
     //exit(EXIT_FAILURE);
 }
@@ -581,7 +581,7 @@ void handle_sb(unsigned int cur_inst) {
     /*
      * Lab2-2 assignment
      */
-    unsigned int rs1 = MASK19_15(cur_inst)
+    unsigned int rs1 = MASK19_15(cur_inst);
     unsigned int rs2 = MASK24_20(cur_inst);
     int imm12 = MASK11_7(cur_inst) + (MASK31_25(cur_inst) << 5);
     int8_t tmp = CURRENT_LATCHES.REGS[rs2];
@@ -595,7 +595,7 @@ void handle_sh(unsigned int cur_inst) {
     /*
      * Lab2-2 assignment
      */
-    unsigned int rs1 = MASK19_15(cur_inst)
+    unsigned int rs1 = MASK19_15(cur_inst);
     unsigned int rs2 = MASK24_20(cur_inst);
     int imm12 = MASK11_7(cur_inst) + (MASK31_25(cur_inst) << 5);
     int16_t tmp = CURRENT_LATCHES.REGS[rs2];
@@ -609,7 +609,7 @@ void handle_sw(unsigned int cur_inst) {
     /*
      * Lab2-2 assignment
      */
-    unsigned int rs1 = MASK19_15(cur_inst)
+    unsigned int rs1 = MASK19_15(cur_inst);
     unsigned int rs2 = MASK24_20(cur_inst);
     int imm12 = MASK11_7(cur_inst) + (MASK31_25(cur_inst) << 5);
     MEMORY[sext(imm12, 12) + CURRENT_LATCHES.REGS[rs1]] = CURRENT_LATCHES.REGS[rs2];
