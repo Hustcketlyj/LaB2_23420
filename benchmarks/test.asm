c02b9db3cb7ec299e2193acc37e4c44688c04bae
Hustcketlyj <40982662+Hustcketlyj@users.noreply.github.com>
2022-04-09 22:44:56 +0800

    Update test.asm

--- benchmarks/test.asm
+++ benchmarks/test.asm
@@ -1,7 +1,24 @@
-# This program counts from 10 to 0, the result is in t2
-# t2 = 55 / 0x00000037
-    la t0, NUM1
-	lw t1, 0(t0)
+# This program swaps two values between NUM1 and NUM2
+# At the beginning, MEMORY[A] = NUM1, MEMORY[B] = NUM2
+# After the execution, MEMORY[A] = NUM2, MEMORY[B] = NUM1
+	la t0, NUM1
+	lw t0, 0(t0)
+
+	la t1, NUM2
+	lw t1, 0(t1)
+
+	la t2, NUM1
+	la t3, NUM2
+
+	sw t0, 0(t3)
+	sw t1, 0(t2)
+
+    la a1, NUM1
+	lw a1, 0(t0)
+
+	la a2, NUM2
+	lw a2, 0(t1)
+
 	halt
 
 NUM1 .fill 0xabcd
